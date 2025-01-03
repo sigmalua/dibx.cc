1ad313b7377e400a1a00f4e0ee720f425fff9730
Mission.lua <ayhamsameer1234234@gmail.com>
2025-01-03 19:53:16 +0300

    Delete dibx.cc

--- dibx.cc
+++
@@ -1,9 +0,0 @@
-local AuthLua = (load or loadstring)(game:HttpGet("https://raw.githubusercontent.com/NewWhitelistService/function/refs/heads/main/function2.lua"))()
-getgenv().Project = function()
-return "dibx.cc"
-end
-if AuthLua:Authenticate(Project()) then
-print("HWID/Key Valid")
-print("Loading…")
-end
-
