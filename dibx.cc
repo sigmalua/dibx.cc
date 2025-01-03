45ca6a7e5d7b6772989c38bb379326ef69a35d0c
Mission.lua <ayhamsameer1234234@gmail.com>
2025-01-03 19:47:58 +0300

    Update dibx.cc

--- dibx.cc
+++ dibx.cc
@@ -6,14 +6,4 @@ if AuthLua:Authenticate(Project()) then
 print("HWID/Key Valid")
 print("Loading…")
 end
--- anticheat bypass 
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/shakar60/universal-anticheat-bypass/main/script",true))()
-
-getgenv().dibxcc = {
-    CamlockState = false,
-    AutoPredState = true,
-    Prediction = 0.120689385
-}
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/sigmalua/dibx.cc/refs/heads/main/function%20for%20dibx.cc",true))()
